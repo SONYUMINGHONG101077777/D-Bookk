e79ba5a9c8b328c6d4f9762f0bd7fa917fef1e71
SONYUMINGHONG101077777 <sunyou27071@gmail.com>
2026-01-05 14:17:21 +0700

    Update feature / fix bug

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,22 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./components/ThemeProvider.tsx";
 import { queryClient } from "./lib/queryClient.ts";
 import { QueryClientProvider } from "@tanstack/react-query";
-// import {persister} from "./lib/queryClient.ts"
+
+// ADD more 
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+  onOfflineReady() {
+    console.log('PWA ready to work offline')
+  }
+})
+
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <QueryClientProvider client={queryClient} >
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </QueryClientProvider>
   </BrowserRouter>
 );
